73f21bbdd08295992b3c451e7bc5b43c9e765a4a
Martijn van der Geest <mvandergeest@qualogy.com>
2017-03-28 15:46:05 +0200

    Searching

---
+++ src/main/java/searching/Searching.java
@@ -0,0 +1,19 @@
+package searching;
+
+public class Searching {
+    //2X-SHIFT: Uber search --> show settings.
+    //CTRL-SHIFT-F / CTRL-SHIFT-R --> 1.0
+
+
+    //CTRL-F --> F3 / SHIFT-F3
+    public void someMethodToBeSearchedA() {
+        System.out.println("someString");
+    }
+
+    //CTRL-F (regex) --> [someMethodToBeSearched]
+    public void someMethodToBeSearchedB() {
+        System.out.println("someString");
+    }
+
+    //CTRL-R --> someString --> whatever
+}
---
+++ src/main/java/searching/UsageSearching.java
@@ -0,0 +1,15 @@
+package searching;
+
+public class UsageSearching {
+
+    private InnerUsageSearching innerUsageSearching = new InnerUsageSearching();
+
+    //ALT-F7: Find usages
+    //CTRL-F7: Finf usages in this file
+    //CTRL-SHIFT-F7: Highlist usages in this file
+    //CTRL-ALT-F7: Show usages (dialog?)
+    private class InnerUsageSearching {
+
+    }
+
+}
